17f8e0b18eb58f3d26d166ce7fcd53c07d4589fa
Ilya Boiko <boiko.ilya.evg@gmail.com>
2025-05-23 08:33:12 +0500

    Добавьте файлы проекта.

---
+++ SFyCSm017p6e6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SFyCSm016p6e1;
+
+class Program
+{
+    /// <summary>
+    /// Процедура реализующая основной алгоритм работы программы.
+    /// </summary>
+    static void PerformMain()
+    {
+
+    }
+
+    /// <summary>
+    /// Главная точка входа приложения
+    /// </summary>
+    /// <param name="args">Аргументы командной строки при запуске приложения.</param>
+    static void Main(string[] args)
+    {
+        PerformMain();
+    }
+}
